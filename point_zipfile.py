e2d225fe90a4819dfd8e554d98469ac3046b882f
Betelgeuse <3120002394@mail2.gdut.edu.cn>
2022-09-19 17:45:53 +0800

    third commit

--- point_zipfile.py
+++ point_zipfile.py
@@ -6,7 +6,6 @@ class DECOMPRESS(BaseF):
 
     def Decompress(self):
         zip_file = zipfile.ZipFile(self.source)
-        print("999")
         for names in zip_file.namelist():
             zip_file.extract(names, self.target)
         zip_file.close()
